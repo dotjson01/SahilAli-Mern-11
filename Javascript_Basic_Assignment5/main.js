b66c3f8bed924a15a11317f0762fb5f607b34a76
Dotjson01 <sahilali88827@gmail.com>
2025-11-06 12:55:51 +0530

    Assignment5 Done

---
+++ Javascript_Basic_Assignment5/main.js
@@ -0,0 +1,88 @@
+//     <!-- 
+    
+// Introduction:
+// JavaScript brings interactivity to web pages.
+
+// Objective:
+// Work with variables, data types, and operators.
+// Write basic logic.
+
+// Steps:
+// Create a JS script that takes two numbers and adds them.
+// Use if/else to check if the sum is even or odd.
+// Display the result on the webpage.
+
+// Expected Outcome:
+// A basic interactive webpage powered by JavaScript.
+
+//     -->
+
+
+
+
+/*
+String	      A text of characters enclosed in quotes
+Number	      A number representing a mathematical value
+Bigint	      A number representing a large integer
+Boolean	      A data type representing true or false
+Object	      A collection of key-value pairs of data
+Undefined	  A primitive variable with no assigned value
+Null	      A primitive value representing object absence
+Symbol	      A unique and primitive identifier
+*/
+
+var y = 12;
+console.log(y);
+
+let x=20;
+console.log(x);
+
+const pi= 3.14;
+console.log(pi);
+
+// data types -> String
+let color = "Yellow";
+let lastName = "Johnson";
+console.log(color);
+console.log(lastName);
+
+//data types -> number
+let length = 16;
+let weight = 7.5;
+console.log(length);
+console.log(weight);
+
+// data types -> BigInt 
+let xy = 123456789;
+let yx = 4343343343;
+console.log(xy);
+console.log(yx);
+
+// data types -> boolean
+let yes = true;
+let no = false;
+console.log(yes);
+console.log(no);
+
+//data types -> object 
+const person = {firstname:"Dotjson", lastName: "01"};
+console.log(person);
+console.log(person.firstname)
+
+//data object -> Array object 
+const data = new Date("2022-03-2025");
+console.log(data);
+
+//undefined
+let m;
+let n;
+console.log(m);
+console.log(n);
+
+//symbol
+const l = Symbol();
+const o = Symbol();
+console.log(l);
+console.log(o);
+
+
